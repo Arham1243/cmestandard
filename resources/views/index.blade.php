7208d6674e0cc308f2aab125877c5824ab53f969
Arham1243 <arham404khan@gmail.com>
2024-10-03 18:25:09 +0500

    live Uplolad

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 @section('content')
     
-    <div class="section-wrapper test">
+    <div class="section-wrapper">
         <div class="banner-wrapper">
             <div class="banner">
                 <div class="banner-bg">
